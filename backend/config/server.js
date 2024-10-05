2841c1ace14f62552acd2dbf3deb04048560a821
Investiment Security <richtradereps@gmail.com.com>
2024-10-05 16:07:34 +0100

    Corrigir scripts e adicionar melhorias

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,36 +2,39 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('../routes/auth'); // Volta uma pasta para acessar o auth.js
 require('dotenv').config();
-const express = require('express');
 const cors = require('cors');
 
-// Habilite CORS para o frontend hospedado no GitHub Pages
+// Inicializa o app
+const app = express();
+
+// Habilita CORS para o frontend hospedado no GitHub Pages
 const corsOptions = {
-  origin: 'https://investimentois.github.io',  // seu frontend no GitHub Pages
+  origin: 'https://investimentois.github.io',  // Frontend no GitHub Pages
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 app.use(cors(corsOptions));
 
-// Outros middlewares e rotas aqui...
-
-const app = express();
-
+// Middleware para JSON
 app.use(express.json());
+
+// Rotas de autenticação
 app.use('/api', authRoutes); // Rota base para a autenticação
 
+// Porta do servidor
 const PORT = process.env.PORT || 5000;
 
+// Conexão com o MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('Erro ao conectar ao MongoDB:', err);
   });
-})
-.catch(err => {
-  console.error('Erro ao conectar ao MongoDB:', err);
-});
